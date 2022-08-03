5beb5d43fd48369c78af8c1fdfd35f0e8a04b8de
Mark Page <61083555+MarkPage2k1@users.noreply.github.com>
2022-08-03 18:32:49 +0700

    nginx_php_mysql
    config for serivce "nginx_php_mysql"

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn docker</title>
+</head>
+<body>
+    <h1>Game dễ vler !!!</h1>
+    <?php echo 'Flag: BKCTF{I_l'.random_int(0, 9).'v'.random_int(0, 9).'_DUT}😁😁😁'; ?>
+</body>
+</html>
